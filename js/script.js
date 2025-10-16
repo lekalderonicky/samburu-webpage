f414f11ca99d7fa3916b56c196a7b97651c68c72
lekalderonicky <lekalderonicky@gmail.com>
2025-10-16 14:39:34 +0300

    this is my first commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 // document object model for interactivity
 // click explore samburu should show a msg 
-document.getElementById("explorebtn")
-.addEventListener("click", function(){
-    alert("Welcome, Page under development.")
-})
+document.addEventListener('DOMContentLoaded', function() {
+    const exploreButton = document.getElementById("explorebtn");
+    if (exploreButton) {
+        exploreButton.addEventListener("click", function(){
+            alert("Welcome, Page under development.");
+        });
+    }
+});
